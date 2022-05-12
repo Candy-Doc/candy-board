dafb9351d295e2e477713d0c07fd237983370f98
Maji <steeven.regnault@outlook.fr>
2022-05-12 11:30:08 +0200

    ✨ feat: create candy data types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,7 @@ module.exports = {
     ecmaVersion: 2019,
     sourceType: "module",
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   plugins: ["svelte3"],
   overrides: [
     {
--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
   import { Router } from "svelte-navigator";
+  import "@candy-doc/ui/src/components/Topbar/Topbar";
   import Navbar from "./components/Navbar.svelte";
   import Routes from "./components/Routes.svelte";
+  import { setContext } from "svelte";
+  import { writable } from "svelte/store";
+  import { Context } from "./tools/context";
+  import type { CandyData } from "./types/candyData";
+
+  const candyData = writable<CandyData>([]);
+  setContext(Context.CandyData, candyData);
+
+  async function fetchCandyData() {
+    const response = await fetch("/candy-data.json");
+    const bodyJSON: CandyData = await response.json();
+    $candyData = bodyJSON;
+  }
+  fetchCandyData();
 </script>
 
 <Router>
+  <candy-topbar></candy-topbar>
   <div class="grid grid-cols-3">
     <Navbar />
     <Routes />
