fb41983a4fd7ed656df693cdaa343c1c4ee62c16
Florian Louvet <32623039+Tifloz@users.noreply.github.com>
2022-05-24 11:30:05 +0200

    :construction_worker: Add multiples workflows

---
+++ .releaserc.js
@@ -0,0 +1,71 @@
+const {promisify} = require('util')
+const path = require("path");
+const readFileAsync = promisify(require('fs').readFile)
+const TEMPLATE_DIR = path.join('.github/', 'templates/')
+// Given a `const` variable `TEMPLATE_DIR` which points to "<semantic-release-gitmoji>/lib/assets/templates"
+
+// the *.hbs template and partials should be passed as strings of contents
+const template = readFileAsync(path.join(TEMPLATE_DIR, 'default-template.hbs'))
+const commitTemplate = readFileAsync(
+    path.join(TEMPLATE_DIR, 'commit-template.hbs'))
+
+module.exports = {
+  branches: [
+    'main'
+  ],
+  plugins: [
+    [
+      'semantic-release-gitmoji', {
+      releaseRules: {
+        major: [
+          ':boom:',
+          ':candy:'
+        ],
+        minor: [
+          ':sparkles:',
+          ':zap:',
+          ':lipstick:',
+          ':recycle:'
+        ],
+        patch: [
+          ':bug:',
+          ':arrow_down:',
+          ':arrow_up:',
+          ':rotating_light:'
+        ]
+      },
+      releaseNotes: {
+        template,
+        partials: {commitTemplate},
+        issueResolution: {
+          template: '{baseUrl}/{owner}/{repo}/issues/{ref}',
+          baseUrl: 'https://github.com',
+          source: 'github.com'
+        }
+      }
+    }
+    ],
+    '@semantic-release/github',
+    '@semantic-release/changelog',
+    {
+      changelogFile: 'CHANGELOG.md',
+      changelogTitle: '# Changelog'
+    },
+    '@semantic-release/git',
+    {
+      assets: [
+        'CHANGELOG.md',
+        'package.json'
+      ],
+      message: ':bookmark: chore(release): ${nextRelease.gitTag} \n\n${nextRelease.notes}'
+    },
+    "@semantic-release/npm",
+    {
+      npmPublish: true
+    }
+  ],
+
+  extends: [
+    'semantic-release-config-gitmoji'
+  ]
+}
